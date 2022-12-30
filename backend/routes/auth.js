4a90d06b05109792e2334ade8d880b754ae1775a
Neelesh Aggarwal <86107529+AggarwalNeelesh@users.noreply.github.com>
2022-12-31 01:10:28 +0530

    bcrypt and JWT json web token implemented

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const { body, validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs'); 
 const User = require('../models/User')
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "BlueMoonDevil";
+
 // Create a User using : POST "/api/auth/createuser" . Dosen't require Auth
 // No login required
 router.post('/createuser', [  // Array
@@ -16,21 +20,36 @@ router.post('/createuser', [  // Array
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    //Check whether theuser with this email exist already
-    let user = await User.findOne({email: req.body.email});
-    if(user){
-      return res.status(400).json({error: "User Already exist"});
-    }
-    user =  await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-    user.save();
-    // .then(user => res.json(user))
-    // .catch(err=> console.log(err));
+    try{
+      //Check whether theuser with this email exist already
+      let user = await User.findOne({email: req.body.email});
+      if(user){
+        return res.status(400).json({error: "User Already exist"});
+      }
 
-    res.send(req.body);
+      const salt = await bcrypt.genSalt(10);
+      securedPssword = await bcrypt.hash(req.body.password, salt);
+
+      user =  await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: securedPssword,
+      })
+      user.save();
+
+      const data ={
+        user:{
+          id: user.id
+        }
+      }
+
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json(authToken);
+
+    }catch(error){
+      console.error(error.message);
+      res.status(500).send("Some error Occured");
+    }
 })
 
 module.exports = router
