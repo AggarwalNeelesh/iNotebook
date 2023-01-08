dedc8516c5d38eb942a1284925cdf8ed14d480e5
Neelesh Aggarwal <86107529+AggarwalNeelesh@users.noreply.github.com>
2023-01-09 00:11:39 +0530

    Updaing note api call

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,16 +1,16 @@
 import React, {useContext} from 'react'
 import noteContext from '../context/notes/noteContext'
 const Noteitem = (props) => {
-    const note = props.note;
-    const context = useContext(noteContext);
-    const {deleteNote} = context; // De structuring
+  const context = useContext(noteContext);
+  const {deleteNote} = context; // De structuring
+  const {note, updateNote} = props;
   return (
     <div className="card col-md-3 mx-2 my-3">
     <div className="card-body">
         <h5 className="card-title">{note.title}</h5>
         <p className="card-text">{note.description}</p>
         <i className="fa-regular fa-plus mx-2"></i>
-        <i className="fa-solid fa-pen-to-square mx-2"></i>
+        <i className="fa-solid fa-pen-to-square mx-2"  onClick={()=>{updateNote(note)}}></i>
         <i className="fa-solid fa-trash mx-2" onClick={()=>{deleteNote(note._id)}}></i>
     </div>
     </div>
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,21 +1,71 @@
-import React, { useContext, useEffect} from 'react'
+import React, { useContext, useEffect, useRef, useState} from 'react'
 import noteContext from '../context/notes/noteContext'
 import AddNote from './AddNote';
 import Noteitem from './Noteitem';
 const Notes = () => {
     const context = useContext(noteContext);
     const {notes, getNotes} = context; // De structuring
+    const [note, setNote] = useState({etitle:"", edescription:"", etag:""});
     useEffect(() => {
       getNotes();
     }, [])
-    
+    const ref = useRef(null)
+    const updateNote =(currentNote)=>{
+        ref.current.click();
+        setNote({etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag});
+    }
+
+    const handleClick = (e)=>{
+      console.log("Updating note",note);
+      e.preventDefault();
+    }
+    // Changing state of note when typing in textfield
+    const onChange = (e)=>{
+        // ...note => jo chiz note me phele se hai vo rahe
+        // baki usme ab ye chiz add karo ya overwrite karo
+        setNote({...note, [e.target.name]:e.target.value})
+    }
   return (
     <>
     <AddNote/>
+    <button type="button" className="btn btn-primary d-none" ref={ref} data-bs-toggle="modal" data-bs-target="#exampleModal">
+      Launch demo modal
+    </button>
+
+    <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-body">
+          <form>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Title</label>
+              <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} onChange={onChange} aria-describedby="emailHelp" placeholder="Enter title"/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Description</label>
+              <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} placeholder="Description"/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Tag</label>
+              <input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange} placeholder="tag"/>
+            </div>
+          </form>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
+          </div>
+        </div>
+      </div>
+    </div>
     <div className='row'>
         {notes.map((note) =>{
             // return note.title
-            return <Noteitem key={note._id} note={note}/> ;
+            return <Noteitem key={note._id} updateNote={updateNote} note={note}/> ;
         })}
     </div>
     </>
